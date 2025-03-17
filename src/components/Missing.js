1c9d0ccf84ce84e4db898523d13b4567c95bf3fc
ex1edd <korotkoff.maksim2018@yandex.ru>
2025-03-17 14:27:20 +0700

    missing page + css

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import "./styles/missing.css";
 
-//TODO
 const Missing = () => {
   return (
-    <section style={{ padding: "100px", minHeight: "863px" }}>
-      <p>Страница не найдена</p>
-      <div className="flexGrow">
+    <section className="missing-container">
+      <h1 className="missing-title">Ошибка 404</h1>
+      <p className="missing-text">Страница не найдена</p>
+      <div className="missing-link">
         <Link to="/">Вернуться на главную страницу</Link>
       </div>
     </section>
